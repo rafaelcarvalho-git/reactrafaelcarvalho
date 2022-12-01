c1fa46914a52aa55ac5a8c116ac371acf837d7af
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-12-01 09:39:44 -0300

    commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,19 +1,18 @@
 import './Contact.css'
-import { BsEnvelope, BsPhone, BsGeoAlt, BsChatLeftText } from 'react-icons/bs'
-//import programming from '../../assets/programming.png'
+import { BsEnvelope, BsPhone, BsGeoAlt } from 'react-icons/bs'
 import programming from '../../assets/one.svg'
 
 function Contact() {
     return (
-        <section id="contact" className="bg-dark text-white">
+        <section id="contact" className="bg-dark text-white pt-5">
             <div className="container py-4">
                 <h1 className="text-center fw-bold">Entre em contato</h1>
                 <p className="lead text-center mt-3">Venha bater um papo comigo, estou sempre disponível.</p>
-                <div className="row mt-5">
-                    <div className="col-7 text-center align-items-center my-auto">
+                <div className="contact-me row flex-lg-row flex-column-reverse mt-5 pt-2 pb-3">
+                    <div className="col-lg-7 col-12 text-center align-items-center my-auto">
                         <img className="contact-image img-fluid" src={programming} alt="" />
                     </div>
-                    <div className="col-5">
+                    <div className="col-lg-5 col-12">
                         <div className="contact-item border border-3 rounded-3 pt-4 pb-2 text-center mb-4 mx-auto">
                             <BsPhone className="icons mb-3" />
                             <h4>(88) 98857-3004</h4>
