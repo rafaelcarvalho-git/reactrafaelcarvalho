079ff5975d65655d27dd2dbd99c2c5eb73aed0e0
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-12-29 02:44:57 -0300

    commit

--- src/components/Projetos/Projects-data.js
+++ src/components/Projetos/Projects-data.js
@@ -1,4 +1,12 @@
-export var devicons = [
+import projeto1 from '../../assets/projects-images/portfolio-e2s.png'
+import projeto2 from '../../assets/projects-images/siscert.png'
+import projeto3 from '../../assets/projects-images/react-rick-and-morty.png'
+import projeto4 from '../../assets/projects-images/react-personal-trainer.png'
+import projeto5 from '../../assets/projects-images/react-personal-trainer.png'
+import projeto6 from '../../assets/projects-images/portfolio-academia.png'
+import projeto7 from '../../assets/projects-images/react-football-matches.png'
+
+export const devicons = [
     { id: 1, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' },
     { id: 1, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' },
     { id: 1, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
@@ -16,14 +24,18 @@ export var devicons = [
     { id: 5, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
     { id: 5, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' },
     { id: 6, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
-    { id: 6, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' }
+    { id: 6, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' },
+    { id: 7, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
+    { id: 7, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
+    { id: 7, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' }
 ];
 
-export var projetos = [
-    { id: 1, title: "Portfólio E2S", desc: "Portfólio para exibição dos seus produtos/serviços e informações para contato e localização.", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/e2sweb", link: "https://e2s-corretoradeseguros.web.app/", x: devicons },
-    { id: 2, title: "SisCert E2S", desc: "Sistema Web interno para os parceiros solicitarem certificados digitais e terem informações sobre essas solicitações.", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/e2sweb", link: "https://e2s-corretoradeseguros.web.app/", x: devicons },
-    { id: 3, title: "React Rick and Morty", desc: "Projeto com react para exibir os personagens da série Rick and Morty utilizando a API.", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/rickandmorty", link: "https://reactrickandmorty.web.app/" },
-    { id: 4, title: "React Twitter Trends", desc: "bb", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/e2sweb", link: "https://e2s-corretoradeseguros.web.app/", x: devicons },
-    { id: 5, title: "Personal Trainer", desc: "7aaa", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/e2sweb", link: "https://e2s-corretoradeseguros.web.app/", x: devicons },
-    { id: 6, title: "Academia", desc: "7aaa", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/e2sweb", link: "https://e2s-corretoradeseguros.web.app/", x: devicons }
+export const projetos = [
+    { id: 1, title: "Portfólio E2S", desc: "Portfólio para exibição dos seus produtos/serviços e informações para contato e localização.", image: projeto1, code: "https://github.com/rafaelcarvalho-git/e2sweb", live: "https://e2s-corretoradeseguros.web.app/" },
+    { id: 2, title: "SisCert E2S", desc: "Sistema Web interno para os parceiros solicitarem certificados digitais e terem informações sobre essas solicitações.", image: projeto2, code: "https://github.com/rafaelcarvalho-git/e2sweb", live: "https://e2s-corretoradeseguros.web.app/" },
+    { id: 3, title: "React Rick and Morty", desc: "Projeto com react para exibir os personagens da série Rick and Morty utilizando a API.", image: projeto3, code: "https://github.com/rafaelcarvalho-git/rickandmorty", live: "https://reactrickandmorty.web.app/" },
+    /*{ id: 4, title: "React Twitter Trends", desc: "Projeto com react para exibir os trend topics do Twitter em tempo real.", image: "https://rafaelcarvalho-dev.web.app/img/e2scert.png", code: "https://github.com/rafaelcarvalho-git/twittertrends", live: "https://reacttwittertrends.web.app/" },*/
+    { id: 5, title: "Personal Trainer", desc: "Website Portfólio para Personal Trainer", image: projeto5, code: "", live: "https://templatepersonaltrainer.web.app/" },
+    { id: 1, title: "Academia", desc: "Website Portfólio para Academias", image: projeto6, code: "https://github.com/rafaelcarvalho-git/academia", live: "https://portfolio-academia.web.app/" },
+    { id: 7, title: "React Football Matches", desc: "Projeto com react para exibir partidas de futebol do dia ou ano.", image: projeto7, code: "https://github.com/rafaelcarvalho-git/footballmatches", live: "https://reactfootballmatches.web.app/" }
 ];
