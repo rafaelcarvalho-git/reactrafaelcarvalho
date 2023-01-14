baff8444e6fe9c14bdebc8847abeaef8985210b7
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-01-14 14:57:33 -0300

    commit

--- src/components/Projetos/index.jsx
+++ src/components/Projetos/index.jsx
@@ -1,4 +1,5 @@
 import * as S from './styles'
+import { Button, primaryColor } from '../../global.js'
 import '../../assets/swiper/swiper-bundle.min.css'
 import { Line, TitleSection, Title } from '../../global.js'
 import { BsLink45Deg, BsCodeSlash } from "react-icons/bs"
@@ -9,7 +10,7 @@ import Fade from 'react-reveal/Fade'
 
 function Projetos() {
     return (
-        <S.Projects className="projetos">
+        <S.Projects>
             <Fade>
                 <div className="container">
                     <TitleSection>
@@ -23,7 +24,7 @@ function Projetos() {
                         mousewheel={true}
                         keyboard={true}
                         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                        className="mySwiper mb-4"
+                        className="mb-4"
                     >
                         {projetos.map((projeto) => {
                             return (
@@ -37,15 +38,15 @@ function Projetos() {
                                                 <h5 className="mb-4 mb-lg-0">{projeto.title}</h5>
                                                 <S.ProjectIcons className="ms-lg-auto">
                                                     {devicons.map((icon) => {
-                                                        if (icon.id === projeto.id) { return <img src={icon.icon} width="28px" className="projeto-icon ms-2" alt="project icon" /> }
+                                                        if (icon.id === projeto.id) { return <img src={icon.icon} width="28px" className="ms-2" alt="project-icon" loading="lazy" /> }
                                                     }
                                                     )}
                                                 </S.ProjectIcons>
                                             </div>
                                             <p className="pe-0 pe-lg-5 mb-4">{projeto.desc}</p>
                                             <div className="btn-group btn-group-md mt-4" role="group" aria-label="Basic example">
-                                                <a href={projeto.live} target="_blank" rel="noreferrer noopener" className="btn-pattern info-border px-3"><BsLink45Deg className="me-2" />Live</a>
-                                                <a href={projeto.code} target="_blank" rel="noreferrer noopener" className="btn-pattern info px-3">Code<BsCodeSlash className="ms-2" /></a>
+                                                <Button bgColor={'white'} textColor={primaryColor} href={projeto.live} target="_blank" rel="noreferrer noopener" className="btn-pattern info-border px-3"><BsLink45Deg className="me-2" />Live</Button>
+                                                <Button bgColor={primaryColor} textColor={'white'} href={projeto.code} target="_blank" rel="noreferrer noopener" className="btn-pattern info px-3">Code<BsCodeSlash className="ms-2" /></Button>
                                             </div>
                                         </div>
                                     </div>
