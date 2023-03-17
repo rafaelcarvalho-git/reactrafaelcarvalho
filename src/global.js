0b7c57e7df32db1c8a2735341f76ce8ac8bf186d
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-03-17 14:08:52 -0300

    criando componente container

--- src/global.js
+++ src/global.js
@@ -137,3 +137,30 @@ export const Button = styled.a`
     outline: none;
   }
 `;
+
+export const Container = styled.div`
+  padding-right: calc(1.5rem * .5);
+  padding-left: calc(1.5rem * .5);
+  margin-right: auto;
+  margin-left: auto;
+
+  @media (min-width:576px) {
+      max-width: 540px;
+  }
+
+  @media (min-width:740px) {
+      max-width: 720px;
+  }
+
+  @media (min-width:992px) {
+      max-width: 960px;
+  }
+
+  @media (min-width:1200px) {
+      max-width: 1140px;
+  }
+
+  @media (min-width:1400px) {
+      max-width: 1320px;
+  }
+`;
