7622d13b7c4fb3f7ca2bd95dc08816ff4689a67a
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-07-18 20:48:27 -0300

    :sparkles: feat: adicionando tema configprovider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ConfigProvider } from 'antd'
+import { primaryColor } from 'global'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: primaryColor,
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 )
