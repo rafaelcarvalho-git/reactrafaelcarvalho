c8ee90b179ba42a789c57829880877fe64672f81
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-07-22 23:47:10 -0300

    :sparkles: feat: atualizando header temporario

---
+++ src/pages/Header/index.jsx
@@ -0,0 +1,59 @@
+import * as S from './styles'
+import { Container } from 'global'
+import { BsGithub, BsWhatsapp, BsLinkedin, BsEnvelope } from 'react-icons/bs'
+import Typed from 'react-typed'
+import Zoom from 'react-reveal/Zoom'
+
+import logo from 'assets/logo.png'
+
+const Header = () => {
+    return (
+        <S.Header>
+            <S.HeaderContent>
+                <Zoom>
+                    <Container>
+                        <S.HeaderSub>Hello, i am</S.HeaderSub>
+                        <S.HeaderTitle>
+                            <S.LetterFloat>
+                                <S.Slash style={{ marginRight: '.5rem' }}>{'<'}</S.Slash>
+                                <S.Letter>R</S.Letter>
+                                <S.Letter>A</S.Letter>
+                                <S.Letter>F</S.Letter>
+                                <S.Letter>A</S.Letter>
+                                <S.Letter>E</S.Letter>
+                                <S.Letter>L</S.Letter>
+                            </S.LetterFloat>
+                            <S.LetterFloat>
+                                <S.Letter>C</S.Letter>
+                                <S.Letter>A</S.Letter>
+                                <S.Letter>R</S.Letter>
+                                <S.Letter>V</S.Letter>
+                                <S.Letter>A</S.Letter>
+                                <S.Letter>L</S.Letter>
+                                <S.Letter>H</S.Letter>
+                                <S.Letter>O</S.Letter>
+                                <S.Slash style={{ marginLeft: '.5rem' }}>{'/>'}</S.Slash>
+                            </S.LetterFloat>
+                        </S.HeaderTitle>
+                        <S.HeaderSubtitle textShadow>
+                            <Typed strings={['Web Developer',
+                                'System Analyst',
+                                'Information Technician',
+                                'Technology Lover ❤']}
+                                typeSpeed={50}
+                                backSpeed={60} loop />
+                        </S.HeaderSubtitle>
+                        <S.HeaderIcons>
+                            <S.Icon target="_blank" rel="noreferrer noopener" href="https://github.com/rafaelcarvalho-git"><BsGithub /></S.Icon>
+                            <S.Icon target="_blank" rel="noreferrer noopener" href="https://api.whatsapp.com/send/?phone=5588988573004&text&app_absent=0"><BsWhatsapp /></S.Icon>
+                            <S.Icon target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/rafaelcarvalho-ti/"><BsLinkedin /></S.Icon>
+                            <S.Icon target="_blank" rel="noreferrer noopener" href="mailto:rafaskyplay@gmail.com"><BsEnvelope /></S.Icon>
+                        </S.HeaderIcons>
+                    </Container>
+                </Zoom>
+            </S.HeaderContent>
+        </S.Header >
+    );
+}
+
+export default Header;
---
+++ src/pages/Header/styles.js
@@ -0,0 +1,171 @@
+import styled from 'styled-components'
+import { textShadow, primaryColor } from 'global.js'
+import header from 'assets/images/bg-image.jpg'
+
+export const Header = styled.header`
+  height: 100vh;
+  position: relative;
+  color: #fff;
+  background: url(${header}) center center / cover fixed no-repeat;
+  display: flex;
+  justify-content: center !important;
+`
+
+export const HeaderContent = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+`
+
+export const HeaderTitle = styled.h1`
+    color: #fff;   
+    font-size: 4.5rem;
+    max-width: 820px;
+    display: flex;
+    margin auto;
+    margin-bottom: 24px;
+    justify-content: center;
+    text-shadow: ${props => (props.textShadow ? textShadow : 'none')};
+
+    @media (max-width: 991px) {
+        font-size: 3.8rem;
+    }
+    
+    @media (max-width: 767px) {
+        font-size: 2.850rem;
+        max-width: 400px;
+    }
+
+    @media (max-width: 534px) {
+        font-size: 2.5rem;  
+    }
+
+    @media (max-width: 471px) {
+        font-size: 2.2rem;
+        max-width: 300px;
+    }
+
+    @media (max-width: 418px) {
+        font-size: 2rem;
+    }
+    
+    @media (max-width: 382px) {      
+        font-size: 1.6rem;
+        max-width: 150px;
+    }
+    
+    @media (max-width: 318px) {
+        font-size: 1.4rem;
+    }
+`
+
+export const HeaderSub = styled.p`
+  font-size: 2rem;
+  font-weight: 300;
+  line-height: 1.1;
+
+  @media (max-width: 534px) {
+    font-size: 1.8rem;
+  }
+
+  @media (max-width: 418px) {
+    font-size: 1.65rem !important;
+  }
+
+  @media (max-width: 382px) {
+    font-size: 24px !important;
+  }
+
+  @media (max-width: 318px) {
+    font-size: 1.32px !important;
+  }
+`
+
+export const HeaderSubtitle = styled.p`
+  font-size: 24px;
+  font-weight: 300;
+`
+
+export const LetterFloat = styled.div`
+  display: flex;
+  margin-left: 1rem !important;
+`
+
+export const Letter = styled.div`
+  cursor: pointer;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  -webkit-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
+  margin-left: 0.8;
+  margin-right: 0.8px;
+
+  @media (min-width: 768px) {
+    &:hover,
+    &:focus,
+    &:active {
+      -webkit-transform: translateY(-8px);
+      transform: translateY(-8px);
+    }
+  }
+`
+
+export const Slash = styled.span`
+  color: ${primaryColor};
+  cursor: pointer;
+  margin: auto;
+`
+
+export const HeaderIcons = styled.div`
+  color: ${primaryColor};
+  cursor: pointer;
+  margin: 48px auto 48px auto;
+  display: flex;
+  max-width: 275px;
+
+  @media (max-width: 471px) {
+    width: 250px !important;
+  }
+
+  @media (max-width: 382px) {
+    width: 230px !important;
+  }
+`
+
+export const Icon = styled.a`
+  font-size: 32px;
+  color: white;
+  text-shadow: 0 0 0.2em black, 0 0 0.2em white !important;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  margin: auto;
+
+  @media (max-width: 534px) {
+    font-size: 30px !important;
+    margin-top: 32px !important;
+  }
+
+  @media (max-width: 471px) {
+    font-size: 30px !important;
+  }
+
+  @media (max-width: 382px) {
+    font-size: 28px !important;
+  }
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: ${primaryColor};
+    -webkit-transform: scale(1.12);
+    transform: scale(1.12);
+  }
+`
